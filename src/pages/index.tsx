32d8d644f8428716f4f98554b95c9e65151304ab
Guilherme David <guilhermedavidrk@icloud.com>
2022-10-24 16:29:11 -0300

    build: Initial config :memo:

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next'
+import styles from '../../styles/Home.module.scss'
+
+const Home: NextPage = () => {
+  return <div className={styles.title}>Hello wolrd</div>
+}
+
+export default Home
