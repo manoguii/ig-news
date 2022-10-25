6f765c899f8842e341bf808d7614df65c482ae76
Guilherme David <guilhermedavidrk@icloud.com>
2022-10-25 19:28:58 -0300

    feat: Add user email to database :zap:

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { fauna } from '../../../services/fauna'
 
 const clientId = process.env.GITHUB_ID as string
 const clientSecret = process.env.GITHUB_SECRET as string
@@ -11,4 +13,34 @@ export default NextAuth({
       clientSecret,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email as string),
+                ),
+              ),
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email as string),
+              ),
+            ),
+          ),
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  },
 })
