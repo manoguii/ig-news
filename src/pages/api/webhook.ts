488df1bd6778e388740b8c5ba00afc0992d8dc5b
Guilherme David <guilhermedavidrk@icloud.com>
2022-10-26 18:57:37 -0300

    feat: Listening to stripe webhooks events :zap:

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,7 +1,53 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { Readable } from 'stream'
+import Stripe from 'stripe'
+import { stripe } from '../../services/stripe'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('Evento recebido')
+async function buffer(readable: Readable) {
+  const chunks = []
 
-  return res.status(200).json({ ok: true })
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+  }
+
+  return Buffer.concat(chunks)
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+const relevantEvents = new Set(['checkout.session.completed'])
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const buf = await buffer(req)
+    const secret = req.headers['stripe-signature']
+
+    let event: Stripe.Event
+
+    try {
+      // eslint-disable-next-line no-unused-vars
+      event = stripe.webhooks.constructEvent(
+        buf,
+        secret as string[],
+        process.env.STRIPE_WEBHOOK_SECRET as string,
+      )
+    } catch (err) {
+      return res.status(400).send(`Webhook error ${err}`)
+    }
+
+    const { type } = event
+
+    if (relevantEvents.has(type)) {
+      console.log('Evento recebido', event)
+    }
+
+    res.json({ received: true })
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('method not allowed')
+  }
 }
