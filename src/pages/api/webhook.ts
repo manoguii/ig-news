7ce9fa5bf6566a78cf92d485efe3a6d2a2ba2e4d
Guilherme David <guilhermedavidrk@icloud.com>
2022-10-26 19:42:44 -0300

    feat: Saving subscription data to the database :zap:

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { Readable } from 'stream'
 import Stripe from 'stripe'
 import { stripe } from '../../services/stripe'
+import { saveSubscription } from './_lib/manageSubscription'
 
 async function buffer(readable: Readable) {
   const chunks = []
@@ -42,7 +43,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event
 
     if (relevantEvents.has(type)) {
-      console.log('Evento recebido', event)
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+            // eslint-disable-next-line no-case-declarations
+            const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+            await saveSubscription(
+              checkoutSession.subscription?.toString() as string,
+              checkoutSession.customer?.toString() as string,
+            )
+
+            break
+          default:
+            throw new Error('Unhandled event')
+        }
+      } catch (err) {
+        return res.json({ error: 'Webhook handler failed.' })
+      }
     }
 
     res.json({ received: true })
