600f6aeb455340a1ece28ba30e91131da20b8a75
Guilherme David <guilhermedavidrk@icloud.com>
2022-10-26 17:37:55 -0300

    fix: Avoid creating duplicate customer in the database :bug:

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,17 +1,48 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
+import { fauna } from '../../services/fauna'
 import { stripe } from '../../services/stripe'
+import { query as q } from 'faunadb'
 
 const SUCCESS_URL = process.env.STRIPE_SUCCESS_URL as string
 const CANCEL_URL = process.env.STRIPE_CANCEL_URL as string
 
+interface UserType {
+  user: {
+    name?: string
+    email?: string
+    image?: string
+  }
+}
+
+interface User {
+  ref: {
+    id?: string
+  }
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const session = await getSession({ req })
+    const session = (await getSession({ req })) as UserType
 
     const stripeCustomer = await stripe.customers.create({
       email: session?.user?.email as string,
     })
+
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email as string),
+        ),
+      ),
+    )
+
+    await fauna.query(
+      q.Update(q.Ref(q.Collection('users'), user.ref.id), {
+        data: { stripe_customer_id: stripeCustomer.id },
+      }),
+    )
 
     const stripeCheckoutSessions = await stripe.checkout.sessions.create({
       customer: stripeCustomer.id,
