31d27de36c4ff4eab82ed7716ffed54290320297
Guilherme David <guilhermedavidrk@icloud.com>
2022-10-27 16:52:11 -0300

    feat: Listening to webhook event to update active or canceled subscription status in database :zap:

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -20,7 +20,12 @@ export const config = {
   },
 }
 
-const relevantEvents = new Set(['checkout.session.completed'])
+const relevantEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscription.created',
+  'customer.subscription.deleted',
+  'customer.subscription.updated',
+])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -45,6 +50,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          case 'customer.subscription.created':
+          case 'customer.subscription.deleted':
+          case 'customer.subscription.updated':
+            // eslint-disable-next-line no-case-declarations
+            const subscription = event.data.object as Stripe.Subscription
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              type === 'customer.subscription.created',
+            )
+            break
           case 'checkout.session.completed':
             // eslint-disable-next-line no-case-declarations
             const checkoutSession = event.data.object as Stripe.Checkout.Session
@@ -52,6 +69,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             await saveSubscription(
               checkoutSession.subscription?.toString() as string,
               checkoutSession.customer?.toString() as string,
+              true,
             )
 
             break
