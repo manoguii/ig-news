8ba4a47456957a8bfeebbde3096093af2e388229
Guilherme David <guilhermedavidrk@icloud.com>
2022-11-02 16:03:24 -0300

    style: Create page posts :art:

---
+++ .slicemachine/assets/slices/Ignews/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Ignews';
+
+export default {
+  title: 'slices/Ignews'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Birth","spans":[]}],"description":[{"type":"paragraph","text":"Quis ea Lorem voluptate dolore sunt ipsum sunt non culpa commodo nisi anim cupidatat. Deserunt incididunt enim est nostrud.","spans":[]}]},"slice_type":"ignews","id":"_Default"}} />
+_Default.storyName = ''
