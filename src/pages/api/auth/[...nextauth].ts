a95a7fac82ba445afd7f3bfd41b7d79f7cf45188
guilherme david <guilhermedavidrk@icloud.com>
2023-03-16 02:08:47 -0300

    :zap: feat: create user in faunadb after login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { query as q } from 'faunadb'
+import { fauna } from '@/services/fauna'
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
@@ -13,6 +15,32 @@ export const authOptions = {
       },
     }),
   ],
+
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        const email = user.email!
+
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email))),
+            ),
+            q.Create(q.Collection('users'), {
+              data: {
+                email: user.email,
+              },
+            }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email))),
+          ),
+        )
+
+        return true
+      } catch (error) {
+        return false
+      }
+    },
+  },
 }
 
 export default NextAuth(authOptions)
